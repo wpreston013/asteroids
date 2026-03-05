53f726fdf5079e14272d34d8049027c4532f5226
wpreston013 <013wpreston@gmail.com>
2026-03-04 21:36:05 -0500

    Creating constants and gui

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -16,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.00
 
 if __name__ == "__main__":
     main()
